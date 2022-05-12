764080599600bcbbb988070f139cb292c7f32614
oluwadare <wintan1418@gmail.com>
2022-05-12 12:21:45 +0100

    Build submit button for stock enquiry

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :users
   root 'rooms#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
