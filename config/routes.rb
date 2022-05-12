0be4d22e440226c5fc3a38d3ff08df780161157e
oluwadare <wintan1418@gmail.com>
2022-05-12 13:34:24 +0100

    Add routes for stock info

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # devise_for :users
   root 'rooms#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
