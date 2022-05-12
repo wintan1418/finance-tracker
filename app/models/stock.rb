9795db8e3583c3b548eeb9a5441a7d1314bd3a2a
oluwadare <wintan1418@gmail.com>
2022-05-12 11:36:56 +0100

    Test out encrypting key in credential.yml

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new( publishable_token: 'Tpk_c5dcaaaa8f28474e93d3b49cdd097d1f',
+                                 endpoint: 'https://sandbox.iexapis.com/v1')
+ 
+    client.price(ticker_symbol)
+   end
 end
