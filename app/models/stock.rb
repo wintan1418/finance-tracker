ea2e1424842b743b4a7c235cc0a7f6bc9dae5560
oluwadare <wintan1418@gmail.com>
2022-05-12 11:44:11 +0100

    Add stock api implementation and encrypt Api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new( publishable_token: 'Tpk_c5dcaaaa8f28474e93d3b49cdd097d1f',
+    client = IEX::Api::Client.new( publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                  endpoint: 'https://sandbox.iexapis.com/v1')
  
     client.price(ticker_symbol)
