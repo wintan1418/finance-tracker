7a587a851341749a86072d10a275958884d827b6
oluwadare <wintan1418@gmail.com>
2022-05-26 16:36:11 +0100

    Implement logic to check for correct symbol and empty input

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,13 +1,18 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-    @stock = Stock.new_lookup(params[:stock]) 
-    render 'users/my_portfolio'
-    else
-      flash[:alert] = "please enter a symbol to search"
-      redirect_to my_portfolio_path 
+      if params[:stock].present?
+          @stock = Stock.new_lookup(params[:stock]) 
+            if @stock
+              render 'users/my_portfolio'
+            else
+            flash[:alert] =" please enter a valid symbol"
+            redirect_to my_portfolio_path  
+            end
+          else
+            flash[:alert] = "please enter a symbol to search"
+            redirect_to my_portfolio_path 
+      end
   end
-end
 
 end  
