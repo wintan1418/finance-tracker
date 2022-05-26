00d23a52a64e373a4e078cf343d8b9a25f1b7627
oluwadare <wintan1418@gmail.com>
2022-05-26 15:57:47 +0100

    Implement logic to return error message for empty check

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,13 @@
 class StocksController < ApplicationController
 
   def search
-   @stock = Stock.new_lookup(params[:stock]) 
+    if params[:stock].present?
+    @stock = Stock.new_lookup(params[:stock]) 
     render 'users/my_portfolio'
+    else
+      flash[:alert] = "please enter a symbol to search"
+      redirect_to my_portfolio_path 
   end
+end
 
 end  
