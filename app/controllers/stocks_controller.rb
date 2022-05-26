634e495443e28fdc907c02795f7f9fe216e227f2
oluwadare <wintan1418@gmail.com>
2022-05-26 22:52:54 +0100

    Add javascript ajax function to handle request

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,10 @@ class StocksController < ApplicationController
       if params[:stock].present?
           @stock = Stock.new_lookup(params[:stock]) 
             if @stock
-              render 'users/my_portfolio'
+              respond_to do |format|
+                format.js { render partial: 'users/results' }
+              end
+            
             else
             flash[:alert] =" please enter a valid symbol"
             redirect_to my_portfolio_path  
