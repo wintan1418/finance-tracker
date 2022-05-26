7e3b7f5b75050b2864ca2372d068a18416789f17
oluwadare <wintan1418@gmail.com>
2022-05-27 00:09:23 +0100

    Test user and stock creation on console

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,8 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
-  has_many :users, through :user_stocks
+  has_many :users, through: :user_stocks
 
-  validates :name, :ticker_symbol, presence: true
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new( publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
