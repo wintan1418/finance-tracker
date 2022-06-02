1caacf56731954a2507e1e0e0632079508ce3158
oluwadare <wintan1418@gmail.com>
2022-06-02 13:09:06 +0100

    Generate route for searching friends

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
